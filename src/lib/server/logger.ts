31df0378e76bddb030b0cec4fc8a7693788e3d64
Steve Crow <steve@smcrow.com>
2025-09-08 19:34:45 -0400

    fix winston

--- src/lib/server/logger.ts
+++ src/lib/server/logger.ts
@@ -2,8 +2,8 @@ import { env } from '$env/dynamic/private';
 import winston from 'winston';
 
 const logger = winston.createLogger({
-	level: env.LOG_LEVEL,
-	format: env.NODE_ENV === 'production' ? winston.format.json() : winston.format.simple(),
+	level: env.LOG_LEVEL || 'info',
+	format: winston.format.combine(winston.format.colorize(), winston.format.simple()),
 	transports: [new winston.transports.Console()]
 });
 
