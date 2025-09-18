634c26cc9f73d01a06048cdbd7a7b4b49539538a
Steve Crow <steve@smcrow.com>
2025-09-17 23:06:29 -0400

    Update /visit route

--- src/routes/visit/+page.server.ts
+++ src/routes/visit/+page.server.ts
@@ -1,30 +1,28 @@
 import type { User } from '$lib/db/schema/users';
 import { redirect } from '@sveltejs/kit';
 
-const CALL_TO_ACTION_ROUTES = [
-	{
-		route: '/visit/get-connected',
-		predicate: (user?: User) => !user
-	},
-	{
-		route: '/visit/become-community-member',
-		predicate: (user?: User) => user?.membership === 'basic'
-	},
-	{
-		route: '/visit/become-a-controller',
-		predicate: (user?: User) => user?.membership === 'community'
-	}
-];
-
 export const load = async ({ locals }) => {
 	const { user } = locals;
 
-	const callToActionRoute = CALL_TO_ACTION_ROUTES.find((route) => route.predicate(user));
-
-	if (callToActionRoute) {
-		return redirect(302, callToActionRoute.route);
+	// No user - need to connect VATSIM account
+	if (!user) {
+		return redirect(302, '/visit/get-connected');
 	}
 
-	// Fallback - should not normally reach here
-	return redirect(302, '/visit/get-connected');
+	switch (user.membership) {
+		case 'controller':
+			// Controllers are at the top - no next step needed, redirect to get-connected
+			return redirect(302, '/visit/get-connected');
+
+		case 'community':
+			// Community members can become controllers (regardless of division)
+			return redirect(302, '/visit/become-a-controller');
+
+		case 'basic':
+			return redirect(302, '/visit/become-community-member');
+
+		default:
+			// User has VATSIM account but no membership - start with get-connected
+			return redirect(302, '/visit/get-connected');
+	}
 };
