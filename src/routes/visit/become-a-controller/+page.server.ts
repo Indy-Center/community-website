afc954c016132a5d0c0e2a95b09ca73071feb895
Steve Crow <steve@smcrow.com>
2025-09-17 22:57:47 -0400

    Add gate for transfer

--- src/routes/visit/become-a-controller/+page.server.ts
+++ src/routes/visit/become-a-controller/+page.server.ts
@@ -16,6 +16,12 @@ export const actions = {
 	addVisitor: async ({ locals, request }) => {
 		const { user } = locals;
 
+		const { canVisit } = await checkTransferChecklist(user.cid);
+
+		if (!canVisit) {
+			return { success: false, error: 'You are not eligible to visit.' };
+		}
+
 		await addVisitor(user.cid);
 
 		return { success: true };
